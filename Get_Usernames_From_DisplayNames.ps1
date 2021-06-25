a75dc569f8d949084dd4e941bbb2c463bb2d965e
Boston-of-Gilead <Boston-of-Gilead@users.noreply.github.com>
2021-06-25 10:29:12 -0400

    Add files via upload

---
+++ Get_Usernames_From_DisplayNames.ps1
@@ -0,0 +1,6 @@
+﻿$list = Import-Csv C:\path_to\list.csv
+ForEach($user in $list){    
+$dn = $user.user
+Get-ADUser -Filter { displayName -like $dn } | Select samAccountName | Export-Csv C:\path_to\export2.csv -NoTypeInformation -Append
+}
+
